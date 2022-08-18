ae966389caa25414e9012895f3b4d836ce6414aa
Usr_Prime <>
2022-08-18 13:57:05 -0300

    3.1 - Criar ViewController/ViewModel

---
+++ Parking/Parking/Sections/Register User/RegisterUserViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterUserViewController.swift
+//  Parking
+//
+//  Created by Usr_Prime on 18/08/22.
+//
+
+import UIKit
+
+class RegisterUserViewController: DefaultViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ Parking/Parking/Sections/Register User/RegisterUserViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RegisterUserViewModel.swift
+//  Parking
+//
+//  Created by Usr_Prime on 18/08/22.
+//
+
+import Foundation
+
+class RegisterUserViewModel {
+    
+}
