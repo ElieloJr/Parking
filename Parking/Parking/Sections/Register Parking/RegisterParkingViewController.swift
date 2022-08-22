cb6cbe599319ed647db433f242ec82de65657e94
Usr_Prime <>
2022-08-22 11:42:09 -0300

    4.1 - Criar ViewController/ ViewModel

---
+++ Parking/Parking/Sections/Register Parking/RegisterParkingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterParkingViewController.swift
+//  Parking
+//
+//  Created by Usr_Prime on 22/08/22.
+//
+
+import UIKit
+
+class RegisterParkingViewController: DefaultViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Parking/Parking/Sections/Register Parking/RegisterParkingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RegisterParkingViewModel.swift
+//  Parking
+//
+//  Created by Usr_Prime on 22/08/22.
+//
+
+import Foundation
+
+class RegisterParkingViewModel {
+    
+}
