e8b149e95ea984ed282e7429200aed1e374ba09d
Usr_Prime <>
2022-08-23 14:38:42 -0300

    5.1 - Criar ViewController/ ViewModel

---
+++ Parking/Parking/Sections/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Parking
+//
+//  Created by Usr_Prime on 23/08/22.
+//
+
+import UIKit
+
+class HomeViewController: DefaultViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Parking/Parking/Sections/Home/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  Parking
+//
+//  Created by Usr_Prime on 23/08/22.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+}
