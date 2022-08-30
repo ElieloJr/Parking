50651046cac7a34c9ad5e08a1effd2e8d8bd4c9e
Usr_Prime <>
2022-08-30 16:29:17 -0300

    7.1 - Criar a ViewController/ViewModel

---
+++ Parking/Parking/Sections/Register Vehicle/RegisterVehicleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterVehicleViewController.swift
+//  Parking
+//
+//  Created by Usr_Prime on 30/08/22.
+//
+
+import UIKit
+
+class RegisterVehicleViewController: DefaultViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ Parking/Parking/Sections/Register Vehicle/RegisterVehicleViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RegisterVehicleViewModel.swift
+//  Parking
+//
+//  Created by Usr_Prime on 30/08/22.
+//
+
+import Foundation
+
+class RegisterVehicleViewModel {
+    
+}
