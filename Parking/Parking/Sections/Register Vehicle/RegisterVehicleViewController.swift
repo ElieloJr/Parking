d8bbc97c2b300165837414b1b4aaace6da9f902c
Usr_Prime <>
2022-08-30 16:47:21 -0300

    7.3 - Criar input de Dono

--- Parking/Parking/Sections/Register Vehicle/RegisterVehicleViewController.swift
+++ Parking/Parking/Sections/Register Vehicle/RegisterVehicleViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class RegisterVehicleViewController: DefaultViewController {
 
+    private lazy var ownerName = ParkingTextField(text: "Dono", type: .Normal)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,13 +21,18 @@ class RegisterVehicleViewController: DefaultViewController {
     
     private func setupView() {
         title = "Cadastrar Veículo"
+        
+        view.addSubview(ownerName)
     }
     
-    private func setupUI() {
-        
-    }
+    private func setupUI() { }
     
     private func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            ownerName.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+            ownerName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            ownerName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            ownerName.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 }
