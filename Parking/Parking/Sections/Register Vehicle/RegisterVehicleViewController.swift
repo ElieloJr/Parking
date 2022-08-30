4287829cf565583bffad92253538bf1be64093db
Usr_Prime <>
2022-08-30 16:51:06 -0300

    7.4 - Criar input de Modelo

--- Parking/Parking/Sections/Register Vehicle/RegisterVehicleViewController.swift
+++ Parking/Parking/Sections/Register Vehicle/RegisterVehicleViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class RegisterVehicleViewController: DefaultViewController {
 
     private lazy var ownerName = ParkingTextField(text: "Dono", type: .Normal)
+    private lazy var model = ParkingTextField(text: "Modelo (Nome do Carro)", type: .Normal)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +24,7 @@ class RegisterVehicleViewController: DefaultViewController {
         title = "Cadastrar Veículo"
         
         view.addSubview(ownerName)
+        view.addSubview(model)
     }
     
     private func setupUI() { }
@@ -32,7 +34,12 @@ class RegisterVehicleViewController: DefaultViewController {
             ownerName.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
             ownerName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             ownerName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            ownerName.heightAnchor.constraint(equalToConstant: 50)
+            ownerName.heightAnchor.constraint(equalToConstant: 50),
+            
+            model.topAnchor.constraint(equalTo: ownerName.bottomAnchor, constant: 10),
+            model.leadingAnchor.constraint(equalTo: ownerName.leadingAnchor),
+            model.trailingAnchor.constraint(equalTo: ownerName.trailingAnchor),
+            model.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
