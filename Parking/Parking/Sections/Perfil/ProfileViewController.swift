cb4efeb6b3bd9a10531eafa86871290e2ca632e0
Usr_Prime <>
2022-09-02 14:55:31 -0300

    9.3 - Criar label de "Estacionamento" e "Nome do Estacionamento"

--- Parking/Parking/Sections/Perfil/ProfileViewController.swift
+++ Parking/Parking/Sections/Perfil/ProfileViewController.swift
@@ -11,6 +11,8 @@ class ProfileViewController: DefaultViewController {
     
     private lazy var arrowButton = ParkingBackButton()
     private lazy var userImageView = ParkingLogoImageView()
+    private lazy var parkingLabel = ParkingLabel(content: "Estacionamento", size: 16, type: .lightGreyMessage)
+    private lazy var parkingNameLabel = ParkingLabel(content: "Nome do Estacionamento", size: 18, type: .darkGreyMessage)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +26,8 @@ class ProfileViewController: DefaultViewController {
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: arrowButton)
         
         view.addSubview(userImageView)
+        view.addSubview(parkingLabel)
+        view.addSubview(parkingNameLabel)
     }
     
     private func setupUI() {
@@ -35,7 +39,14 @@ class ProfileViewController: DefaultViewController {
             userImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.width/4.2),
             userImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             userImageView.widthAnchor.constraint(equalToConstant: view.frame.width/3.2),
-            userImageView.heightAnchor.constraint(equalToConstant: view.frame.width/3.2)
+            userImageView.heightAnchor.constraint(equalToConstant: view.frame.width/3.2),
+            
+            parkingLabel.topAnchor.constraint(equalTo: userImageView.topAnchor, constant: 20),
+            parkingLabel.leadingAnchor.constraint(equalTo: userImageView.trailingAnchor, constant: 10),
+            
+            parkingNameLabel.topAnchor.constraint(equalTo: parkingLabel.bottomAnchor),
+            parkingNameLabel.leadingAnchor.constraint(equalTo: parkingLabel.leadingAnchor),
+            parkingNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
         ])
     }
     
