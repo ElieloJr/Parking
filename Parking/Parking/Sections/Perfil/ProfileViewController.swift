80ada850d0a95ca2ff6e07f6a083399732879b65
Usr_Prime <>
2022-09-02 14:44:25 -0300

    9.2 - Criar o campo de img do Cliente e backButton

--- Parking/Parking/Sections/Perfil/ProfileViewController.swift
+++ Parking/Parking/Sections/Perfil/ProfileViewController.swift
@@ -8,10 +8,39 @@
 import UIKit
 
 class ProfileViewController: DefaultViewController {
+    
+    private lazy var arrowButton = ParkingBackButton()
+    private lazy var userImageView = ParkingLogoImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupView()
+        setupUI()
+        setupConstraints()
+    }
+    
+    private func setupView() {
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: arrowButton)
+        
+        view.addSubview(userImageView)
+    }
+    
+    private func setupUI() {
+        arrowButton.addTarget(self, action: #selector(backButton), for: .touchUpInside)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            userImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.width/4.2),
+            userImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            userImageView.widthAnchor.constraint(equalToConstant: view.frame.width/3.2),
+            userImageView.heightAnchor.constraint(equalToConstant: view.frame.width/3.2)
+        ])
+    }
+    
+    @objc func backButton() {
+        dismiss(animated: true, completion: nil)
     }
 
 }
